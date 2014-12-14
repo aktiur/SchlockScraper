a3a4818965b7b3e9f66aee018470abd088796c4f
Arthur Cheysson <arthur@cheysson.fr>
2014-12-14 17:15:46 +0100

    Added new reading shortcuts (see README)

--- README.md
+++ README.md
@@ -21,8 +21,18 @@ It's a bit messy, but here's how you do it for now:
 How to read
 -----------
 
-Get to one of the pages in your favourite browser.
-You can use J and K to navigate between comics.
+Get to one of the numbered pages in your favourite browser.
+Each book is entirely contained in one page. I'm currently thinking 
+how I could intelligently cut down these pages (by chapter? by year?).
 
-You can also set a bookmark to a specific comic with the link at the bottom of it.
-You can use the link at the top of the page or the B key to get to the bookmark.
+You can use J and K to navigate to the next or previous comic, just like it works
+on Facebook or 9gag.
+
+I also implemented another browsing mode which should be more efficient.
+D and F can be used to scroll down and up in a smart way, getting to the next comic
+if it is small enough to be shown entirely or the screen, or showing progressively longer
+comics. This way, you never need to use the mouse or the arrow keys.
+
+You can set a bookmark to a specific comic with the link at the bottom of it.
+You can use the link at the top of the page or the B key to go to the bookmark, even
+if you're currently on a different page.
--- resources/script.js
+++ resources/script.js
@@ -1,22 +1,24 @@
+var scrollSpeed = 200;
+
 function scrollToNew () {
-  scrollTop = $(window).scrollTop();
+  var scrollTop = $(window).scrollTop();
   $('h4').each(function(i, h4){ // loop through article headings
-    h4top = $(h4).offset().top; // get article heading top
+    var h4top = $(h4).offset().top; // get article heading top
     if (scrollTop+10 < h4top) { // compare if document is below heading
-      $.scrollTo(h4, 300); // scroll to in .8 of a second
+      $.scrollTo(h4, scrollSpeed); // scroll to in .8 of a second
       return false; // exit function
     }
   });
 }
 
 function scrollToLast () {
-  scrollTop = $(window).scrollTop();
+  var scrollTop = $(window).scrollTop();
 
   var scrollToThis = null;
 
   // Find the last element with class 'new' that isn't on-screen:
   $('h4').each(function(i, h4) {
-    h4top = $(h4).offset().top;
+    var h4top = $(h4).offset().top;
     if (scrollTop > h4top) {
       // This one's not on-screen - make a note and keep going:
       scrollToThis = h4;
@@ -28,8 +30,86 @@ function scrollToLast () {
 
   // If we found an element in the loop above, scroll to it:
   if(scrollToThis != null) {
-    $.scrollTo(scrollToThis, 300);
+    $.scrollTo(scrollToThis, scrollSpeed);
   }
+}
+
+function scrollDown() {
+	// determine active div
+	var scrollTop = $(window).scrollTop();
+	var windowHeight = $(window).height();
+	
+	var activeDiv = null;
+	var divH = 0;
+	var divT = 0;
+	
+	var scrolling = 0;
+	
+	$('div.comic, h3').each(function(i, div) {
+		divH = $(div).height();
+		divT = $(div).offset().top;
+		if((scrollTop+windowHeight/2 >= divT) && (scrollTop+windowHeight/2 <= divT + divH)) {
+			activeDiv = div;
+			return false;
+		}
+	});
+	
+	if(divT + divH > scrollTop + windowHeight) {
+		// if bottom of comic not visible
+		scrolling = Math.min(divT+divH-.9*windowHeight, scrollTop+.75*windowHeight)
+		$.scrollTo(scrolling, scrollSpeed, {axis:'y'});
+	} else {
+		// if bottom of comic is visible, jump to next
+		activeDiv = $(activeDiv).nextAll('div.comic');
+		if(activeDiv.length > 0) {
+			divT = $(activeDiv).offset().top;
+			divH = $(activeDiv).height();
+			if(divH > windowHeight) {
+				scrolling = divT - .1*windowHeight;
+			} else {
+				scrolling = divT + (divH-windowHeight)/2;
+			}
+			$.scrollTo(scrolling, scrollSpeed, {axis:'y'});
+		}
+	}
+}
+
+function scrollUp() {
+	// determine active div
+	var scrollTop = $(window).scrollTop();
+	var windowHeight = $(window).height();
+	
+	var activeDiv = null;
+	var divH = 0;
+	var divT = 0;
+	
+	var scrolling = 0;
+	
+	$('div.comic, h3').each(function(i, div) {
+		divH = $(div).height();
+		divT = $(div).offset().top;
+		if((scrollTop+windowHeight/2 >= divT) && (scrollTop+windowHeight/2 <= divT + divH)) {
+			activeDiv = div;
+			return false;
+		}
+	});
+	
+	if(divT < scrollTop) { // if top of comic not visible
+		scrolling = Math.max(divT-.1*windowHeight, scrollTop-.75*windowHeight)
+		$.scrollTo(scrolling, scrollSpeed, {axis:'y'});
+	} else { // if top of comic is visible, jump to previous
+		activeDiv = $(activeDiv).prevAll('div.comic');
+		if(activeDiv.length > 0) {
+			divT = $(activeDiv).offset().top;
+			divH = $(activeDiv).height();
+			if(divH > windowHeight) {
+				scrolling = divT+divH - .9*windowHeight;
+			} else {
+				scrolling = divT + (divH-windowHeight)/2;
+			}
+			$.scrollTo(scrolling, scrollSpeed, {axis:'y'});
+		}
+	}	
 }
 
 jQuery(function () {
@@ -41,6 +121,12 @@ jQuery(function () {
     } else if (evt.keyCode == 75) { // K key
       evt.preventDefault();
       scrollToLast();
+	} else if (evt.keyCode == 68) { // D key
+	  evt.preventDefault();
+	  scrollDown();
+	} else if (evt.keyCode == 70) { // F key
+	  evt.preventDefault();
+	  scrollUp();
     } else if (evt.keyCode == 66) { // B key
 	  evt.preventDefault();
 	  if(localStorage.getItem('bookmark')) {
